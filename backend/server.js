88d0e406a34fedc707f08077f25db6f2d95f440a
Udayraj <110400788+UdayrajJhala@users.noreply.github.com>
2025-02-18 01:21:57 +0530

    self ping

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const problemRoutes = require("./routes/problemRoutes");
 const progressRoutes = require("./routes/progressRoutes");
+const axios = require("axios");
 
 require("dotenv").config();
 
@@ -25,8 +26,26 @@ initDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/problem", problemRoutes);
-app.use("/api/progress",progressRoutes);
+app.use("/api/progress", progressRoutes);
 
+app.get("/ping", (req, res) => {
+  res.status(200).send("Pong");
+});
+
+const pingServer = () => {
+  setInterval(() => {
+    axios
+      .get(`http://localhost:${process.env.PORT || 5000}/ping`)
+      .then((response) => {
+        console.log("Pinged the server successfully:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error pinging the server:", error);
+      });
+  }, 600000); 
+};
+
+pingServer(); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
